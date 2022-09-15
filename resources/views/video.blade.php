526a8cd03567b934c70234b073ac416a663b8441
XXBIM <akabimgaming@gmail.com>
2022-09-15 13:30:43 +0700

    oke

---
+++ resources/views/video.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.app')
+
+@section('content')
+<iframe width="1024" height="576" src="https://www.youtube.com/embed/J16qIo_BrZI" title="Devil Fruit from One Piece | Anime with Alvin" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+@endsection
